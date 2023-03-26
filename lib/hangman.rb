fbc20dc6d4740f5dad4120978580286b2841b308
Devon <122610607+DevonTheDeveloper@users.noreply.github.com>
2023-03-26 21:03:32 +0200

    Add colorize & player input handler
    This commit adds the colorize gem, and allows player to enter input which the program also handles.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
-# Module: DictionaryWordSelector
-#
+require 'colorize'
+
 # This module provides a method for generating a secret word by selecting a random word from a provided dictionary file.
-# The dictionary file is expected to be a text file containing a list of words, with one word per line.
-# Words in the dictionary file must be between 5 and 12 characters in length to be considered valid.
 module DictionaryWordSelector
   def generate_secret_word
     dictionary = File.read('dictionary.txt').split
@@ -14,15 +12,50 @@ module DictionaryWordSelector
       valid_words << word if word.to_s.length >= 5 && word.to_s.length <= 12
     end
 
-    secret_word = valid_words.sample
-    return secret_word
+    valid_words.sample
+  end
+end
+
+module PlayerInputHandler
+  def get_player_input
+    puts "Enter a letter, or enter ‘save’ to save progress:\n"
+    player_input = gets.chomp.downcase
+
+    until valid_player_input?(player_input)
+      system('clear')
+      puts "Enter a letter, or enter ‘save’ to save progress:\n"
+      player_input = gets.chomp.downcase
+    end
+    if player_input == 'save'
+      save_game_progress
+    end
+    player_input
+  end
+
+  def valid_player_input?(player_input)
+    if player_input.match?(/^[a-z]{1}$/) || player_input == 'save'
+      true
+    end
+  end
+
+  def save_game_progress
+    system('clear')
+    puts "Feature has not been implemented yet.\n".red
+    get_player_input
+    # puts "Name your save file:\n"
+    # save_file_name = gets.chomp
+    # puts 'Game has been saved!'
   end
 end
 
 class Game
   include DictionaryWordSelector
+  include PlayerInputHandler
 
   def initialize
     @secret_word = generate_secret_word
+    get_player_input
   end
 end
+
+Game.new
